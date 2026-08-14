b26c22f1cfe550085a9267dee6529046d90b2b8a
Sudheer <sudheer.devsol200@gmail.com>
2026-08-14 14:17:19 +0500

    Update Variable files with code

---
+++ PHP/Basics/variables.php
@@ -0,0 +1,50 @@
+<?php
+    /* Variables: Variables are used to store data. it's like a container to put something, now that 
+    something will be the value 
+    
+    -> Variable Naming Rules
+    valid way to define variables
+    * $name
+    * $_name
+    * $name123
+    * $customerName
+    * $customer_name
+    * $_9876
+    
+    Invalid way to define variables
+    * $9name
+    * $&name
+    * $0
+
+    Rules:
+        Must start with $.
+        First character after $ must be a letter or _.
+        Cannot start with a number.
+        Cannot contain special characters except _.
+
+    Note: In PHP Variable names are case-sensitive.
+    $name = "Ali";
+    $NAME = "Ahmed";
+    Both are different
+    
+    */
+
+    //Data Types
+    //PHP supports several data types.
+    $name = "Ali";      // String
+    $age = 20;          // Integer
+    $marks = 88.5;      // Float
+    $isPass = true;     // Boolean
+    $data = null;       // NULL
+    
+    // echo $name;
+    // echo $age;
+    // echo $marks;
+    // echo $isPass;
+    // echo $data;
+
+    echo "<br>";
+    
+    // We can print all in one shot as 
+    echo "The Student $name whose age is $age he scored $marks in exams.";
+?>
